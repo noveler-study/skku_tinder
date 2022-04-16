f987aab31093e51158e0d9f44ba05f81fa68e70d
noveler-study <103440394+noveler-study@users.noreply.github.com>
2022-04-16 11:11:42 +0900

    스프링부트 세팅

---
+++ src/test/java/com/skku_tinder/demo/SkkuTinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skku_tinder.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkkuTinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
